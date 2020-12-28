c8cf18986cdb235ce3fa426ea15b4bffcf4d100a
Vyctor <dev.vyctor@gmail.com>
2020-12-28 19:06:20 -0300

    estruturas sequenciais

---
+++ src/curso_java/Main.java
@@ -0,0 +1,20 @@
+package curso_java;
+
+import java.math.*;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        ExerciciosSequencial exerciciosSequencial = new ExerciciosSequencial();
+        // exerciciosSequencial.exercicio1();
+        // exerciciosSequencial.exercicio2();
+        // exerciciosSequencial.exercicio3();
+        // exerciciosSequencial.exercicio4();
+        //exerciciosSequencial.exercicio5();
+        exerciciosSequencial.exercicio6();
+    }
+}
+
